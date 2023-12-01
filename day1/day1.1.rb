d3c4c2525f9c54f1ebe274aa72b67b5b09c10304
Sergio Diaz Alvarez <sergio@rubiconmd.com>
2023-12-01 14:21:15 +0100

    Day 1.1

---
+++ day1/day1.1.rb
@@ -0,0 +1,13 @@
+def final_digit(digits)
+  final_digit = digits.first + digits.last
+  final_digit.to_i
+end
+
+total = 0
+File.readlines('1.input', chomp: true).each do |line|
+  digits = line.chars.filter{|x| x.match?(/\d+/) }
+  total += final_digit(digits)
+  puts "#{digits} #{final_digit(digits)}"
+end
+
+puts total
